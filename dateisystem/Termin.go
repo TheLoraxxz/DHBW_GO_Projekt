3ac649092f826245912c14f84413e9ac34f87d38
GunJesus <christian.rohlfs@web.de>
2022-11-03 16:55:28 +0100

    Termin Struct finished
    Termin hält jetzt:
    Titel
    Beschreibung
    Wiederkehrend
    Datum
    Endedatum
    
    Time: Eingabe nur in form von: "2007-03-02T15:02:05 UTC" sonst fickt dich Go

--- dateisystem/Termin.go
+++ dateisystem/Termin.go
@@ -1,5 +1,9 @@
 package dateisystem
 
+import (
+	time "time"
+)
+
 type repeat int
 
 const (
@@ -10,10 +14,36 @@ const (
 	niemals
 )
 
+const (
+	dateLayoutISO = "2006-01-02T15:04:05 UTC"
+)
+
 type Termin struct {
 	title       string
 	description string
 	recurring   repeat
-	time        string
-	date        string
+	date        time.Time
+	endDate     time.Time
+}
+
+func setTitle(t *Termin, newTitle string) {
+	t.title = newTitle
+}
+
+func setDescription(t *Termin, newDescription string) {
+	t.description = newDescription
+}
+
+func setRecurring(t *Termin, newRecurring repeat) {
+	t.recurring = newRecurring
+}
+
+func setDate(t *Termin, newDate string) {
+	d, _ := time.Parse(dateLayoutISO, newDate)
+	t.date = d
+}
+
+func setEndeDate(t *Termin, newDate string) {
+	d, _ := time.Parse(dateLayoutISO, newDate)
+	t.endDate = d
 }
--- dateisystem/Termin_test.go
+++ dateisystem/Termin_test.go
@@ -1,16 +1,26 @@
 package dateisystem
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func newTermin(title string) *Termin {
 	t := Termin{title: title}
 	t.description = "test"
 	t.recurring = repeat(niemals)
-	t.time = "15:04"
-	t.date = "2022-10-03"
+	t.date = time.Date(2021, 8, 15, 14, 30, 45, 0, time.UTC)
+	t.endDate = time.Date(2021, 8, 15, 15, 30, 45, 0, time.UTC)
 	return &t
+}
+
+func updateTermin(termin *Termin) {
+	setTitle(termin, "testo")
+	setDescription(termin, "testo")
+	setRecurring(termin, repeat(woechentlich))
+	setDate(termin, "2007-03-02T13:02:05 UTC")
+	setEndeDate(termin, "2007-03-02T15:02:05 UTC")
 }
 
 func TestTermin(t *testing.T) {
@@ -19,16 +29,41 @@ func TestTermin(t *testing.T) {
 	if termin.title == "" {
 		t.Errorf("Titel nicht hinterlegt")
 	}
-	if termin.date == "" {
-		t.Errorf("Datum nicht hinterlegt")
-	}
-	if termin.time == "" {
-		t.Errorf("Zeit nicht hinterlegt")
-	}
 	if termin.description == "" {
 		t.Errorf("Beschreibung nicht hinterlegt")
 	}
 	if termin.recurring != 4 {
 		t.Errorf("Wiederholung nicht hinterlegt")
+	}
+	if termin.date.String() == "" {
+		t.Errorf("Datum nicht hinterlegt")
+	}
+
+	if termin.endDate.String() != "2021-08-15 15:30:45 +0000 UTC" {
+		t.Errorf("End-Datum nicht hinterlegt")
+	}
+}
+
+func TestTerminUpdate(t *testing.T) {
+	termin := newTermin("test")
+	updateTermin(termin)
+
+	if termin.title == "test" {
+		fmt.Println(termin.title)
+		t.Errorf("Titel nicht aktualisiert")
+	}
+	if termin.description == "test" {
+		fmt.Println(termin.description)
+		t.Errorf("Beschreibung nicht aktualisiert")
+	}
+	if termin.recurring == repeat(niemals) {
+		fmt.Println(termin.recurring)
+		t.Errorf("Wiederholung nicht aktualisiert")
+	}
+	if termin.date.String() == "2021-08-15 14:30:45 +0000 UTC" {
+		t.Errorf("Datum nicht aktualisiert")
+	}
+	if termin.endDate.String() == "2021-08-15 15:30:45 +0000 UTC" {
+		t.Errorf("Datum nicht aktualisiert")
 	}
 }
