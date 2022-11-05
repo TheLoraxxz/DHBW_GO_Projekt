ad755f8eec2ba4a8c068f3a1bb9dcbd351494e75
the_loraxxz <the_loraxxz@web.de>
2022-11-05 21:08:18 +0100

    server startubg working

--- main_test.go
+++ main_test.go
@@ -1,7 +1,20 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestWebServerStart(t *testing.T) {
+	req := httptest.NewRequest("GET", "localhost:80", nil)
+	recorder := httptest.NewRecorder()
+	RootHandler{}.ServeHTTP(recorder, req)
+	resp := recorder.Result()
+	assert.Equal(t, 200, resp.StatusCode)
+	out, err := io.ReadAll(resp.Body)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, "Hello!", string(out))
 
 }
