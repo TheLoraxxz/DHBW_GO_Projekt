e712201d156bf65dcb2b595dfdf13348a1505a2c
The Loraxxz <tilmann.lorenz@mail.com>
2022-11-08 13:19:52 +0100

    post finally works, i forgot to give forms names
    STUPID

--- templateRouter.go
+++ templateRouter.go
@@ -3,18 +3,14 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
 )
 
 func (h RootHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "POST" {
-		body, err := io.ReadAll(request.Body)
-		fmt.Println(body)
-		if err != nil {
-			log.Fatal("Coudnt export Parsefiles")
-		}
+		fmt.Println(request.FormValue("user"))
+		writer.WriteHeader(404)
 	}
 	mainRoute, err := template.ParseFiles("./assets/templates/index.html", "./assets/templates/header.html", "./assets/templates/footer.html")
 	err = mainRoute.Execute(writer, nil)
