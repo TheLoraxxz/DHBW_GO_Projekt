b2e9d54c50d4e29f5bf7f327964ebfe2c99cc5b2
The Loraxxz <tilmann.lorenz@mail.com>
2022-11-08 10:10:11 +0100

    extracted the router to a different file and working on post request

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 )
 
-type RootHandler struct {
-}
+type RootHandler struct{}
+type LoginHandler struct{}
 
 var Server = http.Server{
 	Addr: ":80",
-}
-
-func (h RootHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	mainRoute, err := template.ParseFiles("./assets/templates/index.html", "./assets/templates/header.html", "./assets/templates/footer.html")
-	err = mainRoute.Execute(writer, nil)
-	if err != nil {
-		log.Fatal("Coudnt export Parsefiles")
-	}
-
 }
 
 func main() {
--- templateRouter.go
+++ templateRouter.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+func (h RootHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		body, err := io.ReadAll(request.Body)
+		fmt.Println(body)
+		if err != nil {
+			log.Fatal("Coudnt export Parsefiles")
+		}
+	}
+	mainRoute, err := template.ParseFiles("./assets/templates/index.html", "./assets/templates/header.html", "./assets/templates/footer.html")
+	err = mainRoute.Execute(writer, nil)
+	if err != nil {
+		log.Fatal("Coudnt export Parsefiles")
+	}
+
+}
