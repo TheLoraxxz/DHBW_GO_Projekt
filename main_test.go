b74d09d62454957fbb386f5a6fbd1a15e33b3298
The Loraxxz <tilmann.lorenz@mail.com>
2022-11-15 10:27:25 +0100

    main tests works now

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"crypto/tls"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -16,12 +15,13 @@ Tests whether web server works even on Port 80
 // test routine um ssl zertifikat zuu testen
 func TestCertificateWorks(t *testing.T) {
 	go main()
-
 	time.Sleep(1 * time.Second)
 	_, err := tls.Dial("tcp", "localhost:80", &tls.Config{InsecureSkipVerify: true})
 	assert.Equal(t, err, nil)
 
-	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
-	assert.Equal(t, Server.Shutdown(ctx), nil)
+	err = Server.Close()
+	if err != nil {
+		assert.Failf(t, "Coudn't Close server", "")
+	}
 
 }
