20b997fc2fc2a7a13ba03a02e666725997deee40
The Loraxxz <tilmann.lorenz@mail.com>
2022-11-25 10:58:53 +0100

    termin is now workable

--- terminfindung/urlToken.go
+++ terminfindung/urlToken.go
@@ -1,25 +1,42 @@
 package terminfindung
 
-import "DHBW_GO_Projekt/dateisystem"
+import (
+	"DHBW_GO_Projekt/dateisystem"
+	"sync"
+)
 
 type User struct {
+	Url       string
 	Name      string
-	Vorschlag map[int64]bool
+	Vorschlag map[string]bool
 }
 type TerminFindung struct {
-	Url              string
-	vorschlagTermine []Vorschlag
+	user             string
+	info             dateisystem.Termin
+	vorschlagTermine []dateisystem.Termin
 	persons          []User
 }
-type Vorschlag struct {
-	id     int64
-	termin dateisystem.Termin
-}
+
 type termine struct {
+	mutex  sync.RWMutex
+	shared []TerminFindung
 }
 
-// make(map[string]Id)
+var allTermine = termine{shared: []TerminFindung{}}
 
 func CreateURLToken(termin *dateisystem.Termin, user *string) {
 
+}
+
+func CreateSharedTermin(termin *dateisystem.Termin, user *string) string {
+	allTermine.mutex.Lock()
+	defer allTermine.mutex.Unlock()
+	newShared := TerminFindung{
+		user:             *user,
+		info:             *termin,
+		vorschlagTermine: []dateisystem.Termin{},
+		persons:          []User{},
+	}
+	allTermine.shared = append(allTermine.shared, newShared)
+	return termin.ID
 }
